dd26a410e161e366da03579757e8230bfb7b6566
James Adams <james@makenub.com>
2012-06-06 14:34:49 -0600

    Initial Commit

---
+++ TNTReceiptPrinter/MNAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MNAppDelegate.h
+//  TNTReceiptPrinter
+//
+//  Created by James Adams on 6/6/12.
+//  Copyright (c) 2012 Pencil Busters, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ TNTReceiptPrinter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TNTReceiptPrinter
+//
+//  Created by James Adams on 6/6/12.
+//  Copyright (c) 2012 Pencil Busters, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MNAppDelegate class]));
+    }
+}
